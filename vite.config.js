236812ee8d9e3d6400d09f0c80226a5cc5b5d9d2
Ajay-710 <ajaybosshoss@gmai.com>
2025-12-08 17:44:51 +0530

    error in live page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vivakids-updated/',
   plugins: [react()],
   resolve: {
     alias: {
