f081e64af3cb448db09bb3f8f95b5c678ddcf941
Ajay-710 <ajaybosshoss@gmai.com>
2025-12-08 17:49:13 +0530

    Fix deployment config for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Admission from './pages/Admission';
